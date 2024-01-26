2f8c20164a3cdfb8b8978a052ff776b17459a868
frectonz <fraol0912@gmail.com>
2024-01-26 12:58:44 +0300

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix flake for fl0";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    }:
+    utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs; {
+        devShells.default = mkShell {
+          packages = [
+            nodejs
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+        };
+
+        formatter = nixpkgs-fmt;
+      });
+}
